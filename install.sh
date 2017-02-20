256db0c776cba500b015318a7a2617245e62c6c8
lightxue <bkmgtp@gmail.com>
2017-02-20 23:50:52 +0800

    完善安装脚本

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 PWD=`pwd`
 
 # install zsh
-apt-get install zsh
+sudo apt-get install zsh
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -13,16 +13,22 @@ mkdir tmp
 git clone https://github.com/wting/autojump.git
 cd autojump
 ./install.py
+cd ..
 
 mv ~/.zshrc ~/.zshrc.bak
-ln -s $PWD/zsh/zhsrc ~/.zshrc
+ln -s $PWD/zsh/zshrc ~/.zshrc
 
 # install tmux
-apt-get install tmux
+sudo apt-get install tmux
 ln -s $PWD/tmux/tmux.conf ~/.tmux.conf
 
 # install vim
 ln -s $PWD/vim/vim ~/.vim
 ln -s $PWD/vim/vimrc ~/.vimrc
 # submodule for vundle
+mkdir vim/vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim.git vim/vim/bundle/Vundle.vim
 # vim +PluginInstall +qall
+
+# install git
+ln -s $PWD/git/gitconfig ~/.gitconfig
