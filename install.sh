4d790c8fdb9310cdc42d81ae97c7b9d1e4c5513f
lightxue <bkmgtp@gmail.com>
2017-03-07 10:42:40 +0800

    apt-get不用按y

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 PWD=`pwd`
 
 # install zsh
-sudo apt-get install zsh
+sudo apt-get install -y zsh
 
 # install oh-my-zsh
+sudo apt-get install -y curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # install autojump
