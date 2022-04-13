31d37d0d4b0d7644379cd91cf5f26266dac14545
lightxue <bkmgtp@gmail.com>
2022-04-13 23:50:00 +0800

    Add zplug
    Signed-off-by: lightxue <bkmgtp@gmail.com>

--- install.sh
+++ install.sh
@@ -9,15 +9,12 @@ sudo apt-get install -y zsh
 # install oh-my-zsh
 sudo apt-get install -y curl
 
-REMOTE='https://github.com.cnpmjs.org/ohmyzsh/ohmyzsh.git' sh -c "$(curl -fsSL https://raw.staticdn.net/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+REMOTE='https://github.com/ohmyzsh/ohmyzsh.git' sh -c "$(curl -fsSL https://raw.staticdn.net/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# 更新子模块
-git submodule init
-git submodule update
+# install zplug
+$ curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
-# install thefuck
 sudo apt-get install -y python3 python3-pip
-sudo pip3 install thefuck
 
 mv ~/.zshrc ~/.zshrc.bak
 ln -s $PWD/zsh/zshrc ~/.zshrc
@@ -33,14 +30,13 @@ test -e ~/.vim && rm -rf ~/.vim.bak && mv ~/.vim ~/.vim.bak
 ln -s $PWD/vim/vim/ ~/.vim
 ln -s $PWD/vim/vimrc ~/.vimrc
 mkdir ~/.vim/bundle
-git clone https://github.com.cnpmjs.org/junegunn/vim-plug.git ~/.vim/bundle/vim-plug --depth=1
+git clone https://github.com/junegunn/vim-plug.git ~/.vim/bundle/vim-plug --depth=1
 
 # install git
 ln -s $PWD/git/gitconfig ~/.gitconfig
 
 # install fzf
-git clone --depth 1 https://github.com.cnpmjs.org/junegunn/fzf.git ~/.fzf --depth=1
-sed -i 's/github.com/github.com.cnpmjs.org/g' ~/.fzf/install
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf --depth=1
 ~/.fzf/install
 
 # handy tools
@@ -48,8 +44,7 @@ sudo apt-get install -y htop glances ack-grep ctags cscope finger tree cloc aria
 # brew install htop glances ctags cscope tree cloc aria2 nnn ripgrep axel cowsay axel
 sudo pip3 install ipython jupyter tldr mycli httpie
 
-#npm install -g diff-so-fancy
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install --lts
+npm install -g diff-so-fancy
 npm install -g bash-language-server
-
-test -e ~/.dotfiles.bin && rm -rf ~/.dotfiles.bin.bak && mv ~/.dotfiles.bin ~/.dotfiles.bin.bak
-ln -s $PWD/bin ~/.dotfiles.bin
