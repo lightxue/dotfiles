d9cfefdd10724808555bcf70795a96114e8a23dc
lightxue <bkmgtp@gmail.com>
2022-07-31 00:30:25 +0800

    程序出退时dap-ui不退出，以便查看标准输出

--- nvim/lua/debugger/init.lua
+++ nvim/lua/debugger/init.lua
@@ -47,12 +47,12 @@ local function configure_exts()
     dap.listeners.after.event_initialized["dapui_config"] = function()
         dapui.open()
     end
-    dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close()
-    end
-    dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close()
-    end
+    -- dap.listeners.before.event_terminated["dapui_config"] = function()
+    --     dapui.close()
+    -- end
+    -- dap.listeners.before.event_exited["dapui_config"] = function()
+    --     dapui.close()
+    -- end
 end
 
 local function configure_debuggers()
