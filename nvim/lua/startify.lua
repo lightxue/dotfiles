af2a89c0b734e3552b4c32d96cec67c624fc2d6e
lightxue <bkmgtp@gmail.com>
2022-08-03 22:08:54 +0800

    调整startify快捷键，展示内容

--- nvim/lua/startify.lua
+++ nvim/lua/startify.lua
@@ -1,21 +1,40 @@
 local M = {}
 
-local function alpha_reset()
-    -- local startify = require'alpha.themes.startify'
+local function init_theme()
     local dashboard = require'alpha.themes.dashboard'
+    local btn = dashboard.button
     dashboard.section.header.val = require('fortune')()
-    require'alpha'.setup(dashboard.opts)
+    dashboard.section.buttons.val = {
+        btn("e", "  New file",     "<Cmd>ene <CR>"),
+        btn("s", "  Scratch",      "<Cmd>Scratch<CR>"),
+        btn("f", "  Find file",    "<Cmd>Telescope find_files<CR>"),
+        btn("m", "  Frecency/MRU", "<Cmd>Telescope oldfiles<CR>"),
+        btn("p", "  Find project", "<Cmd>Telescope projects<CR>"),
+        btn("b", "  Find buffer",  "<Cmd>Telescope buffers<CR>"),
+        btn("g", "  Find word",    "<Cmd>Telescope live_grep<CR>"),
+    }
+    dashboard.section.footer.val = [[
+Vim is a way of life
+  🄻 🄸 🄶 🄷 🅃  🅇 🅄 🄴
+    ]]
+    return dashboard
+end
+
+local function reset_theme(theme)
+    theme.section.header.val = require('fortune')()
+    require'alpha'.setup(theme.opts)
 end
 
 function M.setup()
+    local theme = init_theme()
     vim.api.nvim_create_user_command('Startify', function()
-        alpha_reset()
+        reset_theme(theme)
         require'alpha'.start(false)
     end, {
         bang = true,
         desc = 'require"alpha".start(false)',
     })
-    alpha_reset()
+    reset_theme(theme)
 end
 
 return M
