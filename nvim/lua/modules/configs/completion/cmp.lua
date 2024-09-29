c5c8b2fd1a268f48c6caf03ac759d6f9b80f4379
lightxue <bkmgtp@gmail.com>
2024-09-30 00:47:00 +0800

    cmp不使用tab快捷键，留给copilot
    Signed-off-by: lightxue <bkmgtp@gmail.com>

--- nvim/lua/modules/configs/completion/cmp.lua
+++ nvim/lua/modules/configs/completion/cmp.lua
@@ -136,24 +136,24 @@ return function()
             ['<C-d>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
             ['<C-w>'] = cmp.mapping.close(),
-            ['<Tab>'] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_next_item()
-                elseif require('luasnip').expand_or_locally_jumpable() then
-                    vim.fn.feedkeys(t('<Plug>luasnip-expand-or-jump'))
-                else
-                    fallback()
-                end
-            end, { 'i', 's' }),
-            ['<S-Tab>'] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_prev_item()
-                elseif require('luasnip').jumpable(-1) then
-                    vim.fn.feedkeys(t('<Plug>luasnip-jump-prev'), '')
-                else
-                    fallback()
-                end
-            end, { 'i', 's' }),
+            -- ['<Tab>'] = cmp.mapping(function(fallback)
+            --     if cmp.visible() then
+            --         cmp.select_next_item()
+            --     elseif require('luasnip').expand_or_locally_jumpable() then
+            --         vim.fn.feedkeys(t('<Plug>luasnip-expand-or-jump'))
+            --     else
+            --         fallback()
+            --     end
+            -- end, { 'i', 's' }),
+            -- ['<S-Tab>'] = cmp.mapping(function(fallback)
+            --     if cmp.visible() then
+            --         cmp.select_prev_item()
+            --     elseif require('luasnip').jumpable(-1) then
+            --         vim.fn.feedkeys(t('<Plug>luasnip-jump-prev'), '')
+            --     else
+            --         fallback()
+            --     end
+            -- end, { 'i', 's' }),
         }),
         snippet = {
             expand = function(args)
