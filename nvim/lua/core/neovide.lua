e09a847c5da9493057db66d83849efb9ebe7acd1
lightxue <bkmgtp@gmail.com>
2024-10-11 21:24:23 +0800

    neovide输入时才打开输入法
    Signed-off-by: lightxue <bkmgtp@gmail.com>

--- nvim/lua/core/neovide.lua
+++ nvim/lua/core/neovide.lua
@@ -2,4 +2,28 @@ if not vim.g.neovide then
     return
 end
 
-vim.o.guifont = "Fira Code:h16"
+-- 设置字体
+vim.o.guifont = "Fira Code,MesloLGL Nerd Font Mono,Monaco NF,Monaco:h16"
+
+-- 非输入状态下关闭输入法
+local function set_ime(args)
+    if args.event:match("Enter$") then
+        vim.g.neovide_input_ime = true
+    else
+        vim.g.neovide_input_ime = false
+    end
+end
+
+local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
+
+vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+    group = ime_input,
+    pattern = "*",
+    callback = set_ime
+})
+
+vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+    group = ime_input,
+    pattern = "[/\\?]",
+    callback = set_ime
+})
