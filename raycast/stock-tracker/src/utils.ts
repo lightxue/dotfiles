795d92ad056adc1ae85e8dac28de1d75ede82598
lightxue <bkmgtp@gmail.com>
2025-03-04 21:59:33 +0800

    raycast extenstion
    Signed-off-by: lightxue <bkmgtp@gmail.com>

---
+++ raycast/stock-tracker/src/utils.ts
@@ -0,0 +1,32 @@
+import { Color, Icon } from "@raycast/api";
+
+export function formatMoney(value?: number, currency?: string) {
+  if (!value) {
+    return "—";
+  }
+
+  const magnitude = Math.floor(Math.log10(Math.max(value, 1)) / 3);
+  const suffices = ["", "k", "M", "B", "T"];
+  const suffix = suffices[magnitude];
+  const scaledValue = value / Math.pow(10, magnitude * 3);
+  let strValue = scaledValue.toFixed(2);
+  // if (currency) {
+  //   strValue = new Intl.NumberFormat("en-US", { style: "currency", currency }).format(scaledValue);
+  // }
+  return strValue + suffix;
+}
+
+export function formatPercent(value?: number) {
+  if (!value) {
+    return "—";
+  }
+
+  return value.toFixed(2) + "%";
+}
+
+export function changeIcon(change?: number) {
+  return {
+    source: change ? (change > 0 ? Icon.ArrowUp : Icon.ArrowDown) : Icon.Dot,
+    tintColor: change ? (change > 0 ? Color.Red : Color.Green) : Color.PrimaryText,
+  };
+}
